8f12f2ba6ed96dc7c49d9a5f06e7f29f8380783f
Samirramirodiaz <samir.r.diaz11@gmail.com>
2020-02-19 00:52:57 -0300

    Inicio

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.css">
+    <title>examen PHP</title>
+</head>
+<body>
+    <h2>bienvenidos al imalaia</h2>
+</body>
+</html>
